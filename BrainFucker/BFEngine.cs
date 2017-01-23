5f854f900e46fa20585719928ea154db6cfbeea5
Zak <zwrawr@gmail.com>
2017-01-23 22:52:59 +0000

    Created a brain fuck engine.

--- BrainFucker/BFEngine.cs
+++ BrainFucker/BFEngine.cs
@@ -1,17 +1,214 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿// <copyright file="BFEngine.cs" company="Zak West">
+//     This code is licensed under GNU LGPL v3.0.
+// </copyright>
+// <author>Zak West, @zwrawr, zwrawr@gmail.com</author>
 
 namespace BrainFucker
 {
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// A brain fuck engine, for interpreting brain fuck code.
+    /// </summary>
     public class BFEngine
     {
+        /// <summary>
+        /// The amount of memory that this brain fuck engine has.
+        /// </summary>
         private int dataSize = 30000;
 
-        private struct COMMANDS {
+        /// <summary>
+        /// The memory for this brain fuck engine.
+        /// </summary>
+        private byte[] data;
+
+        /// <summary>
+        /// The data pointer for this brain fuck engine. 
+        /// This points to the current memory cell.
+        /// </summary>
+        private int dataPointer = 0;
+
+        /// <summary>
+        /// The program pointer for this brain fuck engine.
+        /// This points to the current command in the program.        
+        /// </summary>
+        private int programPointer = 0;
+
+        /// <summary>
+        /// The brain fuck program that is being ran.
+        /// </summary>
+        private string program;
+
+        /// <summary>
+        /// The input reader for this brain fuck engine. 
+        /// Inputs to the brain fuck program are obtained from this reader.
+        /// </summary>
+        private StringReader inputReader;
+
+        /// <summary>
+        /// The output writer for this brain fuck engine. 
+        /// Outputs from the brain fuck program are sent over this writer.
+        /// </summary>
+        private StringWriter outputWriter;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BFEngine"/> class. 
+        /// </summary>
+        /// <param name="inputTextReader">The input reader to use.</param>
+        /// <param name="outputTextWriter">The output writer to use.</param>
+        public BFEngine(TextReader inputTextReader, TextWriter outputTextWriter)
+        {
+            this.inputReader = (StringReader)inputTextReader;
+            this.outputWriter = (StringWriter)outputTextWriter;
+
+            this.data = new byte[this.dataSize];
+
+            this.Init();
+        }
+
+        /// <summary>
+        /// Runs a brain fuck program
+        /// </summary>
+        /// <param name="program">The program to be run</param>
+        public void Run(string program)
+        {
+            this.program = program;
+
+            this.Interpret(program.ToCharArray());
+        }
+
+        /// <summary>
+        /// Re runs the last program
+        /// </summary>
+        public void Rerun()
+        {
+            if (this.program != null)
+            {
+                this.Interpret(this.program.ToCharArray());
+            }
+        }
+
+        /// <summary>
+        /// Interpreter for brain fuck. This function is what actually runs brain fuck. 
+        /// </summary>
+        /// <param name="program"> The program to be ran. </param>
+        private void Interpret(char[] program)
+        {
+            int loopDepth = 0;
+            for (this.programPointer = 0; this.programPointer < this.program.Length; this.programPointer++)
+            {
+                switch (program[this.programPointer])
+                {
+                    case COMMANDS.NEXT:
+
+                        this.dataPointer = (this.dataPointer == this.dataSize - 1) ? 0 : this.dataPointer + 1;
+                        break;
+
+                    case COMMANDS.PREV:
+
+                        this.dataPointer = (this.dataPointer == 0) ? this.dataSize - 1 : this.dataPointer - 1;
+                        break;
+
+                    case COMMANDS.INC:
+
+                        this.data[this.dataPointer]++;
+                        break;
+
+                    case COMMANDS.DEC:
+
+                        this.data[this.dataPointer]--;
+                        break;
+
+                    case COMMANDS.OUT:
+
+                        this.outputWriter.Write((char)this.data[this.dataPointer]);
+                        break;
+
+                    case COMMANDS.IN:
+
+                        this.data[this.dataPointer] = (byte)this.inputReader.Read();
+                        break;
+
+                    case COMMANDS.BL:
+
+                        if (this.data[this.dataPointer] == 0)
+                        {
+                            this.programPointer++;
+
+                            while (loopDepth > 0 || program[this.programPointer] != COMMANDS.BR)
+                            {
+                                if (this.program[this.programPointer] == COMMANDS.BL)
+                                {
+                                    loopDepth++;
+                                }
+
+                                if (this.program[this.programPointer] == COMMANDS.BR)
+                                {
+                                    loopDepth--;
+                                }
+
+                                this.programPointer++;
+                            }
+                        }
+
+                        break;
+
+                    case COMMANDS.BR:
+
+                        if (this.data[this.dataPointer] != 0)
+                        {
+                            this.programPointer--;
+
+                            while (loopDepth > 0 || program[this.programPointer] != COMMANDS.BL)
+                            {
+                                if (this.program[this.programPointer] == COMMANDS.BR)
+                                {
+                                    loopDepth++;
+                                }
+
+                                if (this.program[this.programPointer] == COMMANDS.BL)
+                                {
+                                    loopDepth--;
+                                }
+
+                                this.programPointer--;
+                            }
+
+                            this.programPointer--;
+                        }
+
+                        break;
+
+                    default:
+                        throw new Exception("Undefined command");
+                }
+            }
+
+            this.Init();
+        }
+
+        /// <summary>
+        /// Initializes the memory and pointers
+        /// </summary>
+        private void Init()
+        {
+            // zero out memory
+            for (int i = 0; i < this.dataSize; i++)
+            {
+                this.data[i] = 0;
+            }
+
+            // zero pointers
+            this.dataPointer = 0;
+            this.programPointer = 0;
+        }
+
+        /// <summary>
+        /// Used to look up brain fuck commands by there name.
+        /// </summary>
+        private struct COMMANDS
+        {
             public const char NEXT = '>';
             public const char PREV = '<';
             public const char INC = '+';
@@ -20,147 +217,6 @@ namespace BrainFucker
             public const char IN = ',';
             public const char BL = '[';
             public const char BR = ']';
-        }
-
-        private char[] data;
-        private int dataPointer = 0;
-
-
-        private int programPointer = 0;
-
-        private string program;
-
-        private FileStream input;
-        private StreamReader inputReader;
-
-        private FileStream output;
-        private StreamWriter outputWriter;
-
-
-        public BFEngine(FileStream input, FileStream output)
-        {
-            this.data = new char[this.dataSize];
-
-            this.input = input;
-            this.inputReader = new StreamReader(input);
-
-            this.output = output;
-            this.outputWriter = new StreamWriter(output);
-
-        }
-
-        public void run(string program)
-        {
-
-            this.init();
-
-            this.program = program;
-
-            while (programPointer < program.Length)
-            {
-                this.interpret( this.program[programPointer],this.program.ToCharArray() );
-            }
-        }
-
-        private void interpret(char command, char[] program )
-        {
-            int loopDepth;
-
-            switch (command)
-            {
-                case COMMANDS.NEXT:
-
-                    if ((dataPointer+1)< dataSize)
-                    {
-                        dataPointer++;
-                    }
-                    break;
-
-                case COMMANDS.PREV:
-
-                    if ((dataPointer -1 ) > 0)
-                    {
-                        dataPointer--;
-                    }
-                    break;
-
-                case COMMANDS.INC:
-
-                    this.data[dataPointer]++;
-                    break;
-
-                case COMMANDS.DEC:
-
-                    this.data[dataPointer]--;
-                    break;
-
-                case COMMANDS.OUT:
-
-                    this.outputWriter.Write(this.data[dataPointer]);
-                    break;
-
-                case COMMANDS.IN:
-
-                    this.data[dataPointer] = (char)this.inputReader.Read();
-                    break;
-
-                case COMMANDS.BL:
-
-                    if (data[dataPointer] == 0)
-                    {
-                        loopDepth = 1;
-                        while (loopDepth > 0)
-                        {
-                            char futureCommand = program[++programPointer];
-
-                            if (futureCommand == COMMANDS.BL)
-                            {
-                                loopDepth++;
-                            }
-                            else if (futureCommand == COMMANDS.BR)
-                            {
-                                loopDepth--;
-                            }
-                        }
-                    }
-                    break;
-
-                case COMMANDS.BR:
-
-                    loopDepth = 1;
-                    while (loopDepth > 0)
-                    {
-                        char pastCommand = program[--programPointer];
-
-                        if (pastCommand == COMMANDS.BL)
-                        {
-                            loopDepth--;
-                        }
-                        else if (pastCommand == COMMANDS.BR)
-                        {
-                            loopDepth++;
-                        }
-                    }
-                    programPointer--;
-
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        private void init()
-        {
-            // zero out memory
-            for (int i = 0; i < this.dataSize; i++)
-            {
-                this.data[i] = (char)0;
-            }
-
-            // zero pointers
-            this.dataPointer = 0;
-            this.programPointer = 0;
         }
     }
 }
